f4846005c3d027d2e761b890c6f99ce17b81cfd2
Robert Kokorev <robert@kokorev.de>
2023-09-01 17:04:32 +0200

    Complete Spring Security module

---
+++ 40-boot-test/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+e
+5src/test/java/accounts/client/AccountClientTests.java,9/9/996d48cd79f0d99b1eede7eb998190e151ad7f8c
+f
+6src/test/java/accounts/web/AccountControllerTests.java,d/7/d751a24d4393c8a9103685e46e109f4b56e4c959
+j
+:src/test/java/accounts/web/AccountControllerBootTests.java,0/f/0fca9a7ede86a32f2602e6f1f0b23fed73aaf6d5
--- 42-security-rest/src/main/java/config/RestSecurityConfig.java
+++ 42-security-rest/src/main/java/config/RestSecurityConfig.java
@@ -4,6 +4,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -13,10 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-// TODO-10: Enable method security
-// - Add @EnableMethodSecurity annotation to this class
-
 @Configuration
+@EnableMethodSecurity
 public class RestSecurityConfig {
 
 	@Bean
@@ -24,17 +24,10 @@ public class RestSecurityConfig {
 
 		// @formatter:off
         http.authorizeHttpRequests((authz) -> authz
-                // TODO-04: Configure authorization using requestMatchers method
-                // - Allow DELETE on the /accounts resource (or any sub-resource)
-                //   for "SUPERADMIN" role only
-                // - Allow POST or PUT on the /accounts resource (or any sub-resource)
-                //   for "ADMIN" or "SUPERADMIN" role only
-                // - Allow GET on the /accounts resource (or any sub-resource)
-                //   for all roles - "USER", "ADMIN", "SUPERADMIN"
-        		// - Allow GET on the /authorities resource
-                //   for all roles - "USER", "ADMIN", "SUPERADMIN"
-
-                // Deny any request that doesn't match any authorization rule
+						.requestMatchers(HttpMethod.DELETE, "/accounts/**").hasRole("SUPERADMIN")
+						.requestMatchers(HttpMethod.POST, "/accounts/**").hasAnyRole("ADMIN", "SUPERADMIN")
+						.requestMatchers(HttpMethod.PUT, "/accounts/**").hasAnyRole("ADMIN", "SUPERADMIN")
+						.requestMatchers(HttpMethod.GET, "/accounts/**", "/authorities").hasAnyRole("SUPERADMIN", "ADMIN", "USER")
                 .anyRequest().denyAll())
         .httpBasic(withDefaults())
         .csrf(CsrfConfigurer::disable);
@@ -42,22 +35,14 @@ public class RestSecurityConfig {
 
         return http.build();
 	}
-
-	// TODO-14b (Optional): Remove the InMemoryUserDetailsManager definition
-	// - Comment the @Bean annotation below
 	
-	@Bean
+	//@Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-
-		// TODO-05: Add three users with corresponding roles:
-		// - "user"/"user" with "USER" role (example code is provided below)
-		// - "admin"/"admin" with "USER" and "ADMIN" roles
-		// - "superadmin"/"superadmin" with "USER", "ADMIN", and "SUPERADMIN" roles
-		// (Make sure to store the password in encoded form.)
-    	// - pass all users in the InMemoryUserDetailsManager constructor
 		UserDetails user = User.withUsername("user").password(passwordEncoder.encode("user")).roles("USER").build();
+		UserDetails admin = User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("USER", "ADMIN").build();
+		UserDetails superadmin = User.withUsername("superadmin").password(passwordEncoder.encode("superadmin")).roles("USER", "ADMIN", "SUPERADMIN").build();
 
-		return new InMemoryUserDetailsManager(user /* Add new users comma-separated here */);
+		return new InMemoryUserDetailsManager(user, admin, superadmin);
 	}
     
     @Bean
